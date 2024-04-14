4bff66c3d41397b658673e9e9095f17cf9014f68
Aziret055 <kojom5erd1ev1@gmail.com>
2024-04-14 13:44:10 +0600

    finally cinema movie

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,28 +1,62 @@
-import React, { useContext, useState } from "react";
-import Trend from "../Trend";
-import { useNavigate } from "react-router-dom";
+import axios from 'axios'
+import React, { useContext, useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { API_KEY } from '../API'
+import Trend from '../Trend'
+import { lenguageContext } from '../context'
 const Home = () => {
-  const [value, setValue] = useState('')
-  const nav = useNavigate()
-  console.log(value);
-  return (
-    <div id="home">
-      <div className="container">
-        <div className="home">
-          <div className="home-img">
-            <h1>Добро пожаловать.</h1>
-            <h2>Миллионы фильмов, сериалов и людей. Исследуйте сейчас.</h2>
-            <div className="btn-input">
-              <input onChange={(e)=> setValue(e.target.value)} type="text" placeholder="Найти фильм, сериал, персону......"/>
-              <button onClick={()=> nav(`/movieSearch/${value}`)} > Search </button>
-            </div>
-          </div>
-        </div>
-      <Trend/>
-        
-      </div>
-    </div>
-  );
-};
+	const [value, setValue] = useState('')
+	const [popular, setPopular] = useState([])
+	const [page] = useState(1)
+	const { language } = useContext(lenguageContext)
+	const nav = useNavigate()
 
-export default Home;
+	function getPopular(key) {
+		axios(
+			`https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=${language}&page=${page}`
+		).then(res => {
+			setPopular(res.data.results)
+		})
+	}
+	useEffect(() => {
+		getPopular(API_KEY)
+	}, [page, language])
+
+	function randomBackgroundImage() {
+		const randomIndex = Math.floor(Math.random() * popular.length)
+
+		return popular[randomIndex]?.backdrop_path
+	}
+
+	return (
+		<div id='home'>
+			<div className='container'>
+				<div className='home'>
+					<div
+						className='home-img'
+						style={{
+							backgroundImage: `url('https://image.tmdb.org/t/p/original/${randomBackgroundImage()}')`
+						}}
+					>
+						<h1>Добро пожаловать.</h1>
+						<h2>Миллионы фильмов, сериалов и людей. Исследуйте сейчас.</h2>
+						<div className='btn-input'>
+							<input
+								onChange={e => setValue(e.target.value)}
+								type='text'
+								placeholder='Найти фильм, сериал, персону......'
+							/>
+							<button onClick={() => nav(`/movieSearch/${value}`)}>
+								{' '}
+								Search{' '}
+							</button>
+						</div>
+					</div>
+				</div>
+				<Trend />
+			</div>
+		</div>
+	)
+}
+
+export default Home
--- src/components/Popular/index.js
+++ src/components/Popular/index.js
@@ -1,70 +1,82 @@
-import axios from 'axios';
-import React, { useContext, useEffect, useState } from 'react';
-import { API_KEY } from '../API';
-import { Link } from 'react-router-dom';
-import loading from "../../img/LoadingImag.svg"
-import { lenguageContext } from '../context';
+import axios from 'axios'
+import React, { useContext, useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import loading from '../../img/LoadingImag.svg'
+import { API_KEY } from '../API'
+import { lenguageContext } from '../context'
 const Popular = () => {
-    const [popular , setPopular] = useState([])
-    const [languages , setLanguages] = useState("us-US")
-    const [page , setPage] = useState(1)
-    const {language} = useContext(lenguageContext)
-    const {dark} = useContext(lenguageContext)
+	const [popular, setPopular] = useState([])
+	const [languages, setLanguages] = useState('us-US')
+	const [page, setPage] = useState(1)
+	const { language } = useContext(lenguageContext)
+	const { dark } = useContext(lenguageContext)
 
+	function getPopular(key) {
+		axios(
+			`https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=${language}&page=${page}`
+		).then(res => {
+			setPopular(res.data.results)
+		})
+	}
+	useEffect(() => {
+		setTimeout(() => {
+			getPopular(API_KEY)
+		}, 2000)
+	}, [page, language])
 
+	return (
+		<div
+			id='popular'
+			style={{
+				transition: '.8s',
+				background: dark ? 'black' : 'white'
+			}}
+		>
+			<div className='container'>
+				<div className='popular'>
+					{popular.length === 0 ? (
+						<img className='loading' src={loading} alt='img' />
+					) : (
+						popular.map(el => (
+							<div
+								className='pop'
+								style={{
+									color: dark ? 'white' : 'black',
+									border: dark ? '2px solid white' : 'none'
+								}}
+							>
+								<Link to={`/MovieDetails/${el.id}`}>
+									<img
+										src={`https://www.themoviedb.org/t/p/w220_and_h330_face${el.poster_path}`}
+										alt='img'
+									/>
+								</Link>
+								<h2 className='title'>{el.title}</h2>
+								<h3 className='dat'>{el.release_date}</h3>
+							</div>
+						))
+					)}
+				</div>
+			</div>
+			<div
+				style={{
+					display: 'flex',
+					justifyContent: 'center',
+					gap: '30px'
+				}}
+				className='btn'
+			>
+				<button
+					onClick={() => {
+						setPage(page + 1)
+						console.log(page)
+					}}
+				>
+					next
+				</button>
+			</div>
+		</div>
+	)
+}
 
-
-
-    function getPopular(key) {
-      axios(`https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=${language}&page=${page}`).then((res) => {
-            setPopular(res.data.results)
-      })
-    }
-    useEffect(() => {
-     setTimeout(()=> {
-      getPopular(API_KEY)
-     }, 2000)
-    },[page , language])
-console.log(popular);
-
-    return (
-        <div id='popular' style={{
-          transition:".8s",
-          background : dark? "black" : "white"
-        }}>
-            <div className="container">
-                <div className="popular">
-              
-                    {
-                      popular.length === 0 ? <img className='loading' src={loading} alt="img" /> :
-                        popular.map((el) => (
-                           <div className="pop" style={{
-                            color: dark ? 'white' : 'black',
-                            border : dark ? "2px solid white" : "none"
-                           }}>
-                            <Link to={`/MovieDetails/${el.id}`}>
-                            <img src={`https://www.themoviedb.org/t/p/w220_and_h330_face${el.poster_path}`} alt="img" />
-                            </Link>
-                             <h2 className='title'>{el.title}</h2>
-                             <h3 className='dat'>{el.release_date}</h3>
-                           </div>
-                         ) )
-                    }
-                </div>
-            </div>
-              <div style={{
-                display: "flex" ,
-                justifyContent:"center",
-                gap : "30px"
-              }} className="btn">
-                <button onClick={() => {
-                  setPage(page + 1)
-                  console.log(page);
-                }}>next</button>
-         
-              </div>
-        </div>
-    );
-};
-
-export default Popular;
+export default Popular
